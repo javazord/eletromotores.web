aa5957175479b75a1630d4fb98a934b4c3e9c052
mateusmarqsicm <mateusmarqsicm@gmail.com>
2023-03-02 17:19:04 -0300

    refatorando classes para hooks

--- src/views/user/userRegister.js
+++ src/views/user/userRegister.js
@@ -1,89 +1,89 @@
-import React from "react";
 import { Card } from 'primereact/card';
 import UserService from "../../app/service/user/userService";
 import { showMessageSuccess, showMessageError } from "../../components/toastr";
 import HandleInputResetValues from "../../components/events/handleInputResetValues";
-import Row from '../../components/grid/row';
-import Col from '../../components/grid/col';
-import FormGroup from "../../components/grid/form-group";
 import HandleInputChange from "../../components/events/handleInputChange";
 import { Button } from "primereact/button";
-import { Divider } from 'primereact/divider';
+import React, { useState } from 'react';
+import { Row, Col } from 'reactstrap';
 
+const UserRegister = () => {
 
-class UserRegister extends React.Component {
+  const [state, setState] = useState({
+    login: '',
+    password: '',
+    repeatPassword: '',
+    role: 'USER',
+    loading: false
+  });
 
-    state = {
-        login: '',
-        password: '',
-        repeatPassword: '',
-        role: 'USER',
-        loading: false
-    }
+  const service = new UserService();
 
-    constructor() {
-        super();
-        this.service = new UserService();
-    }
+  const create = () => {
+    const { login, password, role } = state;
+    const usuario = { login, password, role }
+    load()
+    service.save(usuario)
+      .then(response => {
+        showMessageSuccess('Usuário cadastrado com sucesso!')
+        handleInputResetValues()
+      }).catch(erro => {
+        console.log(erro)
+        showMessageError(erro.response.data)
+      })
+  }
 
-    create = () => {
-        const { login, password, role } = this.state;
-        const usuario = { login, password, role }
-        this.load()
-        this.service.save(usuario)
-            .then(response => {
-                showMessageSuccess('Usuário cadastrado com sucesso!')
-                HandleInputResetValues()
-            }).catch(erro => {
-                console.log(erro)
-                showMessageError(erro.response.data)
-            })
-    }
+  const load = () => {
+    setState({ ...state, loading: true });
+    setTimeout(() => {
+      setState({ ...state, loading: false });
+    }, 500);
+  };
 
-    load = () => {
-        this.setState({ loading: true });
-        setTimeout(() => {
-            this.setState({ loading: false });
-        }, 500);
-    };
+  const handleInputChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.value })
+  }
 
-    handleInputChange = (event) => {
-        this.setState({ [event.target.name]: event.target.value })
-    }
+  const handleInputResetValues = () => {
+    setState({
+      login: '',
+      password: '',
+      repeatPassword: '',
+      role: 'USER',
+      loading: false
+    });
+  }
 
-    render() {
+  const footer = (
+    <div className="d-flex justify-content-end">
+      <Button label="Cadastrar" icon="pi pi-check" onClick={create} loading={state.loading} size="sm" />
+    </div>
+  );
 
-        const footer = (
-            <div className="d-flex justify-content-end">
-                <Button label="Cadastrar" icon="pi pi-check" onClick={this.create} loading={this.state.loading} size="sm"/>
-            </div>
-        );
-        return (
-            <Row >
-                <Col className="col-md-7 mx-auto">
-                    <Card title={"Cadastrar Colaborador"} footer={footer} >
-                        <Row>
-                            <Col >
-                                <input name="login" value={this.state.login} onChange={this.handleInputChange} className="form-control m-1" placeholder="Login" />
+  return (
+    <Row >
+      <Col className="col-md-7 mx-auto">
+        <Card title={"Cadastrar Colaborador"} footer={footer} >
+          <Row>
+            <Col >
+              <input name="login" value={state.login} onChange={handleInputChange} className="form-control m-1" placeholder="Login" />
 
-                                <select name="role" value={this.state.role} onChange={this.handleInputChange} className="form-select m-1" >
-                                    <option value="USER">Usuario</option>
-                                    <option value="ADMIN">Administrador</option>
-                                </select>
-                            </Col>
-                            <Col>
-                                <input name="password" value={this.state.password} onChange={this.handleInputChange} type="password" className="form-control m-1" placeholder="Senha" />
+              <select name="role" value={state.role} onChange={handleInputChange} className="form-select m-1" >
+                <option value="USER">Usuario</option>
+                <option value="ADMIN">Administrador</option>
+              </select>
+            </Col>
+            <Col>
+              <input name="password" value={state.password} onChange={handleInputChange} type="password" className="form-control m-1" placeholder="Senha" />
 
-                                <input onChange={this.handleInputChange} type="password" className="form-control m-1" placeholder="Repetir senha" />
+              <input name="repeatPassword" value={state.repeatPassword} onChange={handleInputChange} type="password" className="form-control m-1" placeholder="Repetir senha" />
 
-                            </Col>
-                        </Row>
-                    </Card>
-                </Col>
-            </Row>
-
-
-        )
-    }
+            </Col>
+          </Row>
+        </Card>
+      </Col>
+    </Row>
+  )
 }
-export default UserRegister
+
+export default UserRegister;
