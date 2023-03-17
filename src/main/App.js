2d0f25dc455acf6698f5bc2be09d024aa56d3431
mateusmarqsicm <mateusmarqsicm@gmail.com>
2023-03-16 22:00:45 -0300

    alteração no tema

--- src/main/App.js
+++ src/main/App.js
@@ -6,7 +6,7 @@ import MenuBar from '../components/menubar/menubar';
 //import 'bootswatch/dist/minty/bootstrap.css'
 //import 'bootswatch/dist/minty/bootstrap.min.css'
 
-import 'primereact/resources/themes/fluent-light/theme.css';
+import 'primereact/resources/themes/mira/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
